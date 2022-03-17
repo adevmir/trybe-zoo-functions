643293f7c81c634f0f98fd730e7797bc85f20843
adevmir <ademirvalonqar@gmail.com>
2022-03-17 16:46:35 -0300

    [Requisito 4]

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
+const managers = data.employees.map((colab) => colab.managers);
+
 function isManager(id) {
-  // seu código aqui
+  return managers.some(a => a.some(a => a === id) === true);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if ((managers.some(a => a.some(a => a === managerId) === true)) === true) {
+    const colabs = data.employees;
+    const colabsRelateds = colabs.filter(a => a.managers.some(a => a === managerId));
+    return colabsRelateds.map(colabs => `${colabs.firstName} ${ colabs.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
